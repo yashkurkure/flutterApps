f173b6b80df2028c0c90ad114abbc44dd37d73e0
yashkurkure <yash.kurkure1903@gmail.com>
2021-10-24 14:42:30 -0500

    New Apps

---
+++ TouchCircleTrace/java_version/TouchCircleTrace/app/build/generated/source/buildConfig/debug/edu/uic/cs478/ugobuy/touchcircletrace/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.uic.cs478.ugobuy.touchcircletrace;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.uic.cs478.ugobuy.touchcircletrace";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
